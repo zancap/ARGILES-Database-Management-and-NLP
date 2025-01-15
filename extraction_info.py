09a4dde31f32868a60d1672100a300c570ab1ed9
RomainGirardNLP <girardromain06@gmail.com>
2025-01-15 13:02:00 +0100

    Extraction d'info + getter pour 1 audite
    Mise à jour du script d'extraction d'informations à partir des xml.
        - Extraction de tous les xmls du dossier /xml/ par défaut
        - Getter ajouté ( get_audite(id_audite) ) pour obtenir les réponses d'un audite. Renvoie une liste :
    [
       [ document1, [ réponse_évocation, réponse_like, réponse_dislike, réponse_expectations], 
       [ document2, [ réponse_évocation, réponse_like, réponse_dislike, réponse_expectations], 
       [ ... ]
    ]

--- extraction_info.py
+++ extraction_info.py
@@ -13,88 +13,112 @@ Author : Romain Girard
 """
 
 import os,shelve
+import xml.etree.ElementTree as ET
 
-xml_dir = "C:\\Users\\girar\\Documents\\1. Cours\\Projet Professionnel - ARGILES\\transcriptions normalisées ARGILES COMONIMAGE\\"
+# ----------- Fonctions -----------
 
-while not os.path.isdir(xml_dir):
-    xml_dir = input("Veuillez donner le lien vers le dossier des fichiers xml")
+def read_xmls(xml_dir = "xmls/"):
+    """
+        input : Dossier contenant les fichiers xml à lire
 
-files = [f for f in os.listdir(xml_dir) if os.path.isfile(os.path.join(xml_dir, f))]
-document = {}
-groupes = {}
-audites = {}
+        Lit les fichiers xml et récupère les informations si le script est bien organisé selon la DTD (Document > Question > Groupe > Reponse > Idee)
+        Renvoie 3 dictionnaires :
+            Document ['name'] = {id:int, questions: list(str), groupes:list(str) KEY-DOWN}
+            Groupes ['name'] = {id:int, document: str KEY-UP, audites:list(str) KEY-DOWN}
+            Audites ['id'] = {groupe:str KEY-UP, reponses : dict {type:str->reponse:str}}
+            Chaque dictionnaire contient une clé pour passer au dictionnaire supérieur (KEY-UP) / inférieur (KEY-DOWN)
+    """
+    while not os.path.isdir(xml_dir):
+        xml_dir = input("Veuillez donner le lien vers le dossier des fichiers xml")
 
-for dir in files:
-    with open(os.path.join(xml_dir,dir),mode='r',encoding='utf-8') as f:
-        lines = f.readlines()
+    files = [f for f in os.listdir(xml_dir) if os.path.isfile(os.path.join(xml_dir, f))]
+    document = {}
+    groupes = {}
+    audites = {}
 
-        for i,line in enumerate(lines):
-            line = line.strip()
-            #print(line)
-            try:
+    try:
+        for dir in files:
+            with open(os.path.join(xml_dir,dir),mode='r',encoding='utf-8') as f:
+                xml_text = f.read()
+                root = ET.fromstring(xml_text)
 
-                if line[:6] == "<etude":    # <etude idEtude=x sujet=x groupeTemoins=x>
-                    etude_id = line.split('idEtude="')[1].split('"')[0].strip()
-                    etude_sujet = line.split('sujet="')[1].split('"')[0].strip()
-                    etude_groupeTemoins = line.split('groupeTemoins="')[1].split('"')[0].strip()
-                    if sujet in document.keys():
-                        pass
-                    elif sujet != '':
-                        document[etude_sujet] = {"questions":{},
-                            "etude_id":etude_id,
-                            "groupes":[]
-                            }
-
-                if line[:9] == "<question": # <question idQuestion=x texteQuestion=x type=x>
-                    question_id = line.split('idQuestion="')[1].split('"')[0].strip()   # Unused
-                    question_enonce = line.split('texteQuestion="')[1].split('"')[0].strip()
-                    question_type = line.split('type="')[1].split('"')[0].strip()
-                    if question_type not in document[etude_sujet]["questions"].keys():
-                        document[etude_sujet]["questions"]["question_type"] = question_enonce
-
-                if line[:7] == "<groupe":   # <groupe idGroupe=x nomGroupe=x>
-                    groupe_id = line.split('idGroupe="')[1].split('"')[0].strip()
-                    groupe_nom = line.split('nomGroupe="')[1].split('"')[0].strip()
-
-                    if groupe_nom not in document[etude_sujet]["groupes"]:
-                        document[etude_sujet]["groupes"].append(groupe_nom)
-                        groupes[groupe_nom] = {"etude":etude_sujet,
-                            "groupe_id":groupe_id,
-                            "audites":[]
-                            }
-                        
-                if line[:8] == "<reponse":  # <reponse idAudite=x>
-                    id_audite = line.split('idAudite="')[1].split('"')[0].strip()
-                    if id_audite not in groupes[groupe_nom]["audites"]:
-                        groupes[groupe_nom]["audites"].append(id_audite)
-                        audites[id_audite] = {
-                            "groupe":groupe_nom,
-                            "reponses":{}
-                            }
+                # 1. Extraction of document informations
                 
-                if line[:5] == "<idee":     # <idee idIdee=X>XX</idee>
-                    idee_id = line.split('idIdee="')[1].split('"')[0].strip()
-                    idee_line = line.split('>')[1].split('<')[0].strip()
-                    if question_type in audites[id_audite]["reponses"].keys():
-                        audites[id_audite]["reponses"][question_type].append(idee_line)
-                    else:
-                        audites[id_audite]["reponses"][question_type] = [idee_line]
+                ongoing_text = root.attrib['sujet']
 
-            except ValueError as val:
-                print("Value Error : ",val)
-"""
-            except Exception as error:
-                print("Erreur ligne ",i," : ",line," : ",error)
-"""
+                if ongoing_text not in document.keys():
+                    document[ongoing_text] = {
+                        "id":root.attrib['idEtude'],
+                        "questions":{},
+                        "groupes":[]
+                    }
+                else:
+                    raise KeyError("Document déjà existant")
 
 
+                # 2. Extraction of questions informations
+                for question in root:
+                    ongoing_question_type = question.attrib['type']
+                    document[ongoing_text]["questions"][ongoing_question_type] = {"texte":question.attrib['texteQuestion'],"id":question.attrib['idQuestion']}
+
+                    for groupe in question:
+                        ongoing_group = groupe.attrib['nomGroupe']
+                        if ongoing_group not in groupes.keys():
+                            groupes[ongoing_group] = {
+                                'id':groupe.attrib['idGroupe'],
+                                "document":ongoing_text,
+                                "audites":[]
+                            }
+
+                        if ongoing_group not in document[ongoing_text]['groupes']:
+                            document[ongoing_text]['groupes'].append(ongoing_group)
+
+                        for reponse in groupe:
+                            ongoing_audite = reponse.attrib['idAudite']
+                            if ongoing_audite not in audites.keys():
+                                audites[ongoing_audite] = {
+                                    "groupe" : ongoing_group,
+                                    "reponses": {
+                                    }
+                                }
+                            
+                            if ongoing_text not in audites[ongoing_audite]["reponses"].keys():
+                                audites[ongoing_audite]["reponses"][ongoing_text] = {}
+
+                            for idee in reponse:
+                                if ongoing_question_type in audites[ongoing_audite]["reponses"][ongoing_text].keys():
+                                    audites[ongoing_audite]["reponses"][ongoing_text][ongoing_question_type].append(idee.text)
+                                else:
+                                    audites[ongoing_audite]["reponses"][ongoing_text][ongoing_question_type] = [idee.text]
+
+        return document,groupes,audites
+
+    except KeyError as err:
+        print("Erreur : ",err)
+        raise
+    
+def stats(ids_audites):
+    pass
+
+def get_audite(id_audite:str):
+    document,groupes,audites = read_xmls()
+    output = []
+    for text in audites[id_audite]['reponses'].keys():
+        text_reponses = []
+        for key in audites[id_audite]['reponses'][text].keys():
+            text_reponses.append(' '.join(audites[id_audite]['reponses'][text][key]))
+        output.append([text,text_reponses])
+    return output
+
+
+# ---------- Début du script -----------
+
+document,groupes,audites = read_xmls()
+#print(audites)
+print(get_audite('107'))
                 
 
 
 # ---------- Fin du script -----------
 
-for x in [document,groupes,audites]:
-    for key in x.keys():
-        print(key, x[key])
-print(files)
 print('Terminé')
